624e2598269677ae814ceb51159b33b03fcc9382
Greyson Travelstead <gst0001@mix.wvu.edu>
2021-02-10 20:29:26 -0500

    updated nodemailer

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -4,7 +4,7 @@ import React from 'react';
 const ContactForm = () => {
     return (
       <div className="formcontainer">
-      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+      <form name="contact" method="post" data-netlify="true" onSubmit="submit">
         <input type="hidden" name="form-name" value="contact"/>
         <h3 className="contactHeader">Get in touch</h3>    
         <input type="text" name="name" placeholder="Full Name" required />< br/>
